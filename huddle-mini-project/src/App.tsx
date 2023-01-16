baa134a585d78bc25ef8113b5abc1b1b8064d8bb
badmood111 <petrzela.adam@seznam.cz>
2023-01-16 20:31:43 +0100

    make site responsive

--- huddle-mini-project/src/App.tsx
+++ huddle-mini-project/src/App.tsx
@@ -1,32 +1,49 @@
 function App() {
   return (
     <div className="m-12">
-      <img className="w-4/12" src="./images/logo.svg" alt="logo" />
-      <img
-        className="my-16 mx-auto"
-        src="./images/illustration-mockups.svg"
-        alt="illustrations"
-      />
-      <h1 className="mx-12 mb-4 text-center font-sans text-4xl font-semibold tracking-wide text-white">
-        Build The Community Your Fans Will Love
-      </h1>
-      <div className="mx-6 text-center text-2xl font-light text-white">
-        Huddle re-imagines the way we build communities. You have a voice, but
-        so does your audience. Create connections with your users as you engage
-        in genuine discussion.
+      <img className="lg:w-2/12 w-4/12" src="./images/logo.svg" alt="logo" />
+      <div className="lg:flex">
+        <img
+          className="my-16 mx-auto"
+          src="./images/illustration-mockups.svg"
+          alt="illustrations"
+        />
+
+        <div className="lg:my-16">
+          <h1 className="mx-12 mb-4 text-center font-headings text-3xl font-bold tracking-wide text-white lg:text-6xl ">
+            Build The Community Your Fans Will Love
+          </h1>
+          <div className="mx-6 text-center text-xl font-light text-white lg:text-2xl">
+            Huddle re-imagines the way we build communities. You have a voice,
+            but so does your audience. Create connections with your users as you
+            engage in genuine discussion.
+          </div>
+          <button className="mx-auto mt-10 block rounded-full bg-white px-24 py-4 shadow-lg hover:bg-huddle-magenta hover:text-white">
+            Register
+          </button>
+        </div>
       </div>
-      <button className="mx-auto mt-10 block rounded-full bg-white px-24 py-4 shadow-lg">
-        Register
-      </button>
 
       <footer className="my-10 flex justify-center gap-3 text-xl text-white">
-        <a target="blank" href="https://facebook.com" className="relative h-10 w-10 border p-1 rounded-full">
+        <a
+          target="blank"
+          href="https://facebook.com"
+          className="relative h-10 w-10 rounded-full border p-1"
+        >
           <i className="fa-brands fa-facebook-f absolute top-2 left-3"></i>
         </a>
-        <a target="blank" href="https://twitter.com" className="relative h-10 w-10 border p-1 rounded-full">
+        <a
+          target="blank"
+          href="https://twitter.com"
+          className="relative h-10 w-10 rounded-full border p-1"
+        >
           <i className="fa-brands fa-twitter absolute top-2 left-2"></i>
         </a>
-        <a target="blank" href="https://instagram.com" className="relative h-10 w-10 border p-1 rounded-full">
+        <a
+          target="blank"
+          href="https://instagram.com"
+          className="relative h-10 w-10 rounded-full border p-1"
+        >
           <i className="fa-brands fa-instagram absolute top-2 left-2"></i>
         </a>
       </footer>
--- huddle-mini-project/tailwind.config.cjs
+++ huddle-mini-project/tailwind.config.cjs
@@ -3,15 +3,19 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        headings: ["Poppins", "sans-serif"],
+        body: ["Open Sans", "sans-serif"],
+      },
       colors: {
         "huddle-violet": "hsl(257, 40%, 49%)",
         "huddle-magenta": "hsl(300, 69%, 71%)",
       },
       backgroundImage: {
-        "desktop": "url('/images/bg-desktop.svg')",
-        "mobile": "url('/images/bg-mobile.svg')"
+        desktop: "url('/images/bg-desktop.svg')",
+        mobile: "url('/images/bg-mobile.svg')",
       },
     },
   },
-  plugins: [require('prettier-plugin-tailwindcss')],
+  plugins: [require("prettier-plugin-tailwindcss")],
 };
